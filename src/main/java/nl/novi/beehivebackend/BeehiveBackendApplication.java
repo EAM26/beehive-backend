a4287765cc7dbbc496b8391946dc60cd68d8bae9
EAM26 <emuller2607@gmail.com>
2023-06-09 10:14:24 +0200

    initial setup

---
+++ src/main/java/nl/novi/beehivebackend/BeehiveBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.beehivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeehiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeehiveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/beehivebackend/BeehiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.beehivebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeehiveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
