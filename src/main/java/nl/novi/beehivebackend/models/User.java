2ae6fe02429fb440f4c6234c45cbc93d2d38c192
EAM26 <emuller2607@gmail.com>
2024-01-10 16:14:52 +0100

    Cleanup redundant methods

--- src/main/java/nl/novi/beehivebackend/models/User.java
+++ src/main/java/nl/novi/beehivebackend/models/User.java
@@ -18,7 +18,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false)
     private String password;
 
     @Column(nullable = false, unique = true)
@@ -36,33 +36,6 @@ public class User {
     @JsonIgnore
     private Employee employee;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<Authority> getAuthorities() {
-        return authorities;
-    }
 
     public void addAuthority(Authority authority) {
         this.authorities.add(authority);
