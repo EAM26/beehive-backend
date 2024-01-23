ef19c56738d125a2179ce4abb7457237bb3fd7d3
EAM26 <emuller2607@gmail.com>
2024-01-23 12:04:43 +0100

    Add weekDates calculation in RosterService and update RosterOutputDto

--- src/main/java/nl/novi/beehivebackend/dtos/output/RosterOutputDto.java
+++ src/main/java/nl/novi/beehivebackend/dtos/output/RosterOutputDto.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import nl.novi.beehivebackend.models.Shift;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @Setter
 public class RosterOutputDto {
     private String name;
+    private List<LocalDate> weekDates;
     private List<Shift> shifts;
 }
--- src/main/java/nl/novi/beehivebackend/services/RosterService.java
+++ src/main/java/nl/novi/beehivebackend/services/RosterService.java
@@ -8,8 +8,12 @@ import nl.novi.beehivebackend.repositories.RosterRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class RosterService {
@@ -20,8 +24,8 @@ public class RosterService {
         this.rosterRepository = rosterRepository;
     }
 
-    public RosterOutputDto getSingleRoster(String roserName) {
-        Roster roster = rosterRepository.findById(roserName).orElseThrow(() -> new BadRequestException("No Roster found with name: " + roserName));
+    public RosterOutputDto getSingleRoster(String rosterName) {
+        Roster roster = rosterRepository.findById(rosterName).orElseThrow(() -> new BadRequestException("No Roster found with name: " + rosterName));
 
         return transferRosterToOutputDto(roster);
     }
@@ -34,10 +38,30 @@ public class RosterService {
         rosterRepository.delete(roster);
     }
 
+    private List<LocalDate> getDatesOfWeek(String rosterName) {
+        String[] partsName = rosterName.split("-");
+        int weekNumber = Integer.parseInt(partsName[0]);
+        int year = Integer.parseInt(partsName[1]);
+
+//        Get first day of year and adjust to weekNumber and year of rosterName
+        LocalDate startOfWeek = LocalDate.ofYearDay(year, 1)
+                .with(WeekFields.of(Locale.getDefault()).weekOfYear(), weekNumber)
+                .with(TemporalAdjusters.previousOrSame(WeekFields.of(Locale.getDefault()).getFirstDayOfWeek()));
+
+        List<LocalDate> weekDates = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            weekDates.add(startOfWeek.plusDays(i));
+        }
+
+        return weekDates;
+
+    }
+
     private RosterOutputDto transferRosterToOutputDto(Roster roster) {
         RosterOutputDto rosterOutputDto = new RosterOutputDto();
         rosterOutputDto.setName(roster.getName());
         rosterOutputDto.setShifts(roster.getShifts());
+        rosterOutputDto.setWeekDates(getDatesOfWeek(roster.getName()));
         return rosterOutputDto;
     }
 }
